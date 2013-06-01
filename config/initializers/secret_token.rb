41de1a0ef271cc37c1f5b3d895d48e1a7c1884c8
Daniel Heath <daniel.r.heath@gmail.com>
2013-06-01 13:21:13 +1000

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hansearch::Application.config.secret_token = '040871cef530363fb4e3bf039997565e56c5f288343f47de193cba49c0e07c5edfda6e6a540d08259ff43f4280edc862ceae6c6e37ab9c11a87d040f586cdd96'
